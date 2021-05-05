5e20cfc5c93f57c1acf6f2830836cc0d38b6572d
pmatyus <>
2021-05-05 16:08:12 +0000

    password solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    user_password = input('Password?')
+    password = 'Caput Draconis'
+    if (user_password == password):
+      print('Welcome!')
+    else:
+      print('Off with you!')
 
 if __name__ == '__main__':
     main()
